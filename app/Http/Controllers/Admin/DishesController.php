c964902d98117aae0368f8bd8eb3daf91508fb8c
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-02-16 12:35:44 +0100

    aggiunto surname a register

--- app/Http/Controllers/Admin/DishesController.php
+++ app/Http/Controllers/Admin/DishesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
 class DishesController extends Controller
 {
@@ -14,7 +15,7 @@ class DishesController extends Controller
      */
     public function index()
     {
-        //
+
     }
 
     /**
