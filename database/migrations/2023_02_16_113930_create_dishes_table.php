4b5b7642977ac042dc0726c919c85c7a43a6b361
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-02-16 12:54:42 +0100

    creati models dishes e orders e restaurant

---
+++ database/migrations/2023_02_16_113930_create_dishes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('ingredients');
+            $table->text('description');
+            $table->decimal('price', 5, 2);
+            $table->string('photo');
+            $table->boolean('visible')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
---
+++ database/migrations/2023_02_16_114718_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('customer_surname');
+            $table->string('customer_address');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->text('order_note');
+            $table->decimal('total_price', 7, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
