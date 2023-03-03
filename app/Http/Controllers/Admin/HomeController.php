9a2584680de600096d7de3f0391e9cae67c2088d
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-03-03 11:26:58 +0100

    scusa

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,18 +1,5 @@
 <?php
 
-// namespace App\Http\Controllers\Admin;
-
-// use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
-
-// class HomeController extends Controller
-// {
-//     public function index()
-//     {
-//         return view('admin.home');
-//     }
-// }
-
 namespace App\Http\Controllers\Admin;
 use App\User;
 use App\Models\Order;
@@ -25,11 +12,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $user = Auth::user();
-        $orders = Order::whereHas('restaurant', function ($query) use ($user) {
-        $query->where('user_id', $user->id);
-        })->get();
 
-        return view('admin.home',compact('orders'));
+        return redirect()->route('admin.restaurants.index');
     }
 }
