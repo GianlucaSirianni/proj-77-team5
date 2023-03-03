72b8c65d0d5b99fd88d286000bcf2126f561016d
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-03-03 09:59:23 +0100

    merge chri

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -4,7 +4,7 @@
 
 {{-- qui va la create --}}
 
-@foreach ($restaurants as $elem)
+@foreach ($restaurants  as $elem)
 
         <div class="card mb-4">
             <div class="card-header d-flex align-items-center justify-content-between">
@@ -27,10 +27,6 @@
                                 <a class="btn btn-primary" href="{{route('admin.restaurants.edit',$elem->id)}}">
                                     <i class="fa-solid fa-pen"></i>
                                 </a>
-
-                                {{-- <button type="submit" class="btn btn-danger">
-                                    <i class="fa-solid fa-trash"></i>
-                                </button> --}}
                             </div>
                         </div>
                     </form>
@@ -54,7 +50,7 @@
                     </div>
 
                     <div class="ratio" style="--bs-aspect-ratio: 50%;">
-                        <img class="object-fit-md-cover border rounded" src="{{asset("storage/$elem->cover_restaurants")}}" alt="img">
+                        <img class="img-fluid  border rounded" src="{{asset("storage/$elem->cover_restaurants")}}" alt="img">
                     </div>
 
                 </div>
@@ -62,7 +58,37 @@
         </div>
     @endforeach
 
-    {{-- {{$elem->links()}} --}}
+    {{-- dati-ordini --}}
+
+    <table class="table  table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Nome Cliente</th>
+            <th scope="col">Numero telefono</th>
+            <th scope="col">Email</th>
+            <th scope="col">Note</th>
+            <th scope="col">Prezzo</th>
+            <th scope="col">Giorno/Orario Ricezione</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($orders as $elem )
+          <tr>
+            <td>{{$elem->customer_name}}</td>
+            <td>{{$elem->phone_number}}</td>
+            <td>{{$elem->email}}</td>
+            <td>{{$elem->order_note}}</td>
+            <td>{{$elem->total_price}}</td>
+            <td>{{$elem->created_at}}</td>
+          </tr>
+          @endforeach
+        </tbody>
+
+    </table>
+
+
+
+
 
 @endsection
 
